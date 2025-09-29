6207f7c94d9a65cd51f051ca826e12524d283a5d
marianofacultad23 <marianofacultad23@gmail.com>
2025-09-29 01:34:38 -0300

    Add files via upload

---
+++ metodos.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+
+    string nombre;
+
+    nombre = "Mariano" ;
+    cout << nombre<< endl;
+
+   // cout << nombre.length() << endl;
+   // cout << nombre.empty() << endl;
+   // cout << nombre.clear() << endl;
+   // cout << nombre.append( " Eric Julian") << endl;
+   
+/* 
+
+   char pos;
+   pos = nombre.at(0) ;
+   cout << pos ;
+   
+*/
+
+ /*
+
+ nombre.insert ( 7, "'s");
+ cout << nombre; 
+ 
+ */  
+
+ /*
+ 
+ int pos;
+ string nombre1 = "Mariano Programa";
+
+ pos = nombre1.find("Programa");
+ cout << pos;
+ 
+ */
+
+
+ /*
+
+string nombre2 = "Mariano Programa";
+nombre2.erase(8,8 );
+// nombre2.erase(8,-1 );
+cout << nombre2;
+
+
+*/
+
+    return 0;
+
+
+}
